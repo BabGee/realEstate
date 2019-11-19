2abaab9fb331e21c7d468f4c8bb7082f3c0b9d2f
BabGee <gjeremiah170@gmail.com>
2019-11-19 17:43:42 +0300

    backend connected to frontend

--- webcommerce/product/models.py
+++ webcommerce/product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from category.models import Category
 
 class Product(models.Model):
+    image = models.ImageField(default='default.jpg', upload_to='product_pics')
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.IntegerField()
--- webcommerce/webcommerce/settings.py
+++ webcommerce/webcommerce/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'product',
     'category',
+    'testApp',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 LOGIN_REDIRECT_URL = 'globalbiz-home'
